237633e6bb756f4319f2890d8353d4e7d35f6ce7
raviitaliya <italiyaravi704@gmail.com>
2024-04-20 11:12:59 +0530

    get domain from database

--- app.ts
+++ app.ts
@@ -4,4 +4,22 @@ import db from "./db";
 let router = Router();
 
 
+router.get("/domain", async (req, res) => {
+  try {
+    let conn = await db();
+
+    const [results] = await conn.query("select * from mm_domains");
+    res.json({
+      message: "Domain created successfully",
+      data: results,
+    });
+  } catch (error: any) {
+    return res.status(500).send(error.message);
+  }
+});
+
+
+
+
+
 export default router;
