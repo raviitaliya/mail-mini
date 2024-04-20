0d22f92029f9f109fce2c928ee026c58a91c01be
raviitaliya <italiyaravi704@gmail.com>
2024-04-20 11:14:00 +0530

    get user from user table

--- app.ts
+++ app.ts
@@ -44,6 +44,24 @@ router.post("/domain", async (req, res) => {
 
 
 
+router.get("/user", async (req, res) => {
+  try {
+    let conn = await db();
+
+    const [results] = await conn.query("select * from mm_users");
+    res.json({
+      message: "Domain created successfully",
+      data: results,
+    });
+  } catch (error: any) {
+    return res.status(500).send(error.message);
+  }
+});
+
+
+
+
+
 
 
 
